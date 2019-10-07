aaeb6e7dff97f393997df538cc0ba5cb8a00af11
bongomin <bongomindaniel@gmail.com>
2019-10-07 18:24:38 +0300

    feat(change of database string connection)

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
+
 if(process.env.NODE_ENV === 'production'){
-   module.exports ={mongoURI: "mongodb+srv://bongomin:P@55w0rd55@rateme-app-dev-5ajkj.mongodb.net/test?retryWrites=true&w=majority"
+   module.exports ={ mongoURI: 'mongodb+srv://bongomin:P@55w0rd55@rateme-app-dev-5ajkj.mongodb.net/test?retryWrites=true&w=majority'
    
    }
 
 }else{
-   module.exports = {mongoURI :"mongodb://localhost/rateme"}
+   module.exports = {mongoURI :'mongodb://localhost/rateme'}
 
 }
